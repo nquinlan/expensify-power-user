ce881690a724d4c7544875ff2328e6439ba215e0
Nick Quinlan <nick@nicholasquinlan.com>
2015-04-25 15:09:05 -0500

    abstracted autocompletion handling

--- src/inject/document/injected.js
+++ src/inject/document/injected.js
@@ -1,34 +1,48 @@
 (function () {
 	var autocompleteSelector = "select[name=currency],select[name=category],select[name=tag],select[name=reportID],select[name=reportSelector]";
-	var mutationHandler = function (mutationRecords) {
+	var addPowerComplete = function (target) {
+		var target = target || this;
+
+		var $target = $(target);
+
+		$target.data("power-complete", true);
+
+		$target.find(autocompleteSelector).each(function () {
+			$(this).selectToAutocomplete();
+		});
+	};
+	var removePowerComplete  = function (target) {
+		var target = target || this;
+
+		var $target = $(target);
+
+		// If this wasn't the target of power complete, we don't care
+		if($target.data("power-complete") !== true) {
+			return;
+		}
+
+		$target.find(autocompleteSelector).show().siblings("input.ui-autocomplete-input,span[role=status]").off().remove();
+	};
+	var uniqueMutations = function (mutationRecords) {
 		// Find all the mutation event targets
 		var targets = mutationRecords.map(function (record) {
 			return record.target;
 		});
-		// Loop through all the unique targets
-		$.unique(targets).forEach(function (target) {
-			// Look for the now visible dialogs and add autocompletion
-			$(target).filter(".dialog:visible").data("power-complete", true).each(function () {
-				var $this = $(this);
-
-				$this.find(autocompleteSelector).each(function () {
-					$this.selectToAutocomplete();
-				});
-			});
-			$(target).filter(".dialog:hidden").each(function () {
-				var $this = $(this);
-				// If this wasn't the target of power complete, we don't care
-				if($this.data("power-complete") !== true) {
-					return;
-				}
-				$this.find(autocompleteSelector).show().siblings("input.ui-autocomplete-input,span[role=status]").off().remove();
-			});
-		});
+		// Return all the unique targets
+		return $.unique(targets);
 	};
-	var observer = new MutationObserver(mutationHandler);
+	
+	var dialogObserver = new MutationObserver(function (mutationRecords) {
+		uniqueMutations(mutationRecords).forEach(function (target) {
+			// Look for the now visible dialogs and add autocompletion
+			$(target).filter(".dialog:visible").each(addPowerComplete);
+			$(target).filter(".dialog:hidden").each(removePowerComplete);
+		});
+	});
+
 	jQuery(function ($) {
 		$(".dialog").each(function () {
-			observer.observe (this, {attributes: true});
+			dialogObserver.observe(this, {attributes: true});
 		});
 	});
 })();
